63ab66ea8ddaad54b0ec035481219b173339ae48
Emre Keskin <keskinemre71@gmail.com>
2018-11-25 22:57:24 +0300

    edit php blade

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
 var file = require('file-system');
-const reg = /{\s*_(.*?)}/g;
+const reg = /{\s*__(.*?)}/g;
 
 var LangData = {};
-file.recurse('files', ['**/*.html'], function(filepath, relative, filename) {
+file.recurse('../Projects/boomerang_truck/resources', ['**/*.blade.php'], function(filepath, relative, filename) {
   fs.readFile(filepath, 'utf8', (err, data) => {
     if (err) throw err;
     var myArray;
     while ((myArray = reg.exec(data)) !== null) {
       var msg = myArray[1]
-      var str = msg.replace('"','')
-      msg = msg.replace('"','').replace('"','').replace(' ','')
+      console.log(msg)
+      msg = msg.replace(')','').replace('(','').replace('"','').replace('"','').replace("'",'').replace("'",'')
+      console.log(msg)
       LangData[msg] = msg
     }
     fs.writeFile('i18n/tr.i18n.json',JSON.stringify(LangData, null, 2), (err) => {
